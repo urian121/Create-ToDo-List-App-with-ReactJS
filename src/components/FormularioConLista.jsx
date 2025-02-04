c9a88dbc71bf8e4582998b3741fbb94061b34866
URIAN VIERA <urian1213viera@gmail.com>
2025-02-03 20:42:09 -0500

    actualizando libreria

--- src/components/FormularioConLista.jsx
+++ src/components/FormularioConLista.jsx
@@ -4,7 +4,6 @@ import Titulo from "./Titulo";
 
 // Importando la biblioteca nextjs-toast-notify
 import { toast } from "nextjs-toast-notify";
-import "nextjs-toast-notify/dist/nextjs-toast-notify.css";
 
 function FormularioConLista() {
   const [inputNombre, setInputNombre] = useState("");
@@ -53,8 +52,8 @@ function FormularioConLista() {
         progress: true,
         position: "top-right",
         transition: "bounceIn",
-        icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-apple"><path d="M12 20.94c1.5 0 2.75 1.06 4 1.06 3 0 6-8 6-12.22A4.91 4.91 0 0 0 17 5c-2.22 0-4 1.44-5 2-1-.56-2.78-2-5-2a4.9 4.9 0 0 0-5 4.78C2 14 5 22 8 22c1.25 0 2.5-1.06 4-1.06Z"/><path d="M10 2c1 .5 2 2 2 5"/></svg>',
-        sonido: true,
+        icon: "",
+        sound: true,
       });
       setInputNombre("");
       setSelectedCurso("Seleccione el Curso");
@@ -71,6 +70,7 @@ function FormularioConLista() {
     toast.error("Alumno eliminado correctamente", {
       duration: 2000,
       position: "top-center",
+      sound: true,
     });
   };
 
